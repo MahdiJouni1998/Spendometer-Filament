163ddf8de1e42b3c6a6ce824090e1639a7c418fb
Mahdi Jouni <mahdijouni1998@gmail.com>
2023-12-16 22:26:40 +0200

    Currency Filament resource

---
+++ app/Filament/Resources/CurrencyResource.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CurrencyResource\Pages;
+use App\Filament\Resources\CurrencyResource\RelationManagers;
+use App\Models\Currency;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CurrencyResource extends Resource
+{
+    protected static ?string $model = Currency::class;
+
+    protected static ?string $navigationIcon = 'heroicon-m-currency-yen';
+
+    public static function form(Form $form): Form
+    {
+        $currencies_names = getAllCurrenciesNames();
+        $currencies_symbols = getAllCurrenciesSymbols();
+        return $form
+            ->schema([
+                Forms\Components\Select::make('name')
+                    ->required()
+                    ->placeholder('usd, lbp, eur, etc...')
+                    ->options($currencies_names)
+                    ->afterStateUpdated(function (Get $get, Set $set) use ($currencies_symbols) {
+                        $set('symbol', $currencies_symbols[$get('name')]);
+                    })
+                    ->live()
+                    ->unique(),
+                Forms\Components\TextInput::make('symbol')
+                    ->placeholder('$, €, ¥, etc...')
+                    ->required()
+                    ->live()
+                    ->maxLength(20),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->formatStateUsing(fn (string $state) => strtoupper($state))
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('symbol')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCurrencies::route('/'),
+            'create' => Pages\CreateCurrency::route('/create'),
+            'view' => Pages\ViewCurrency::route('/{record}'),
+            'edit' => Pages\EditCurrency::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
