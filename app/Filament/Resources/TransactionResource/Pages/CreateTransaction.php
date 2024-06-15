8396b6158a7573d541b1e66a2c5eeab6f8c4309f
Mahdi Jouni <mahdijouni1998@gmail.com>
2024-06-15 15:20:58 +0300

    Change redirect url for transactions

--- app/Filament/Resources/TransactionResource/Pages/CreateTransaction.php
+++ app/Filament/Resources/TransactionResource/Pages/CreateTransaction.php
@@ -3,12 +3,16 @@
 namespace App\Filament\Resources\TransactionResource\Pages;
 
 use App\Filament\Resources\TransactionResource;
-use App\Models\Balance;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
-use Illuminate\Database\Eloquent\Model;
 
 class CreateTransaction extends CreateRecord
 {
     protected static string $resource = TransactionResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return TransactionResource::getUrl('edit', [
+            'record' => $this->record
+        ]);
+    }
 }
